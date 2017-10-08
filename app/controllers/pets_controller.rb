cff773c22456862564f2a4f8b8dc95fd1ba325c2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 17:24:05 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,12 +4,12 @@ class PetsController < ApplicationController
       @pets = Pet.all
       erb :'/pets/index'
     end
-  
+
     get '/pets/new' do
       @owners = Owner.all
       erb :'/pets/new'
     end
-  
+
     post '/pets' do
       @pet = Pet.create(params[:pet])
       if !params[:owner][:name].empty?
@@ -18,17 +18,17 @@ class PetsController < ApplicationController
       @pet.save
       redirect to "pets/#{@pet.id}"
     end
-  
+
     get '/pets/:id' do
       @pet = Pet.find(params[:id])
       erb :'/pets/show'
     end
-  
+
     get '/pets/:id/edit' do
       @pet = Pet.find(params[:id])
       erb :'/pets/edit'
     end
-  
+
     post '/pets/:id' do
       @pet = Pet.find(params[:id])
       @pet.update(params["pet"])
